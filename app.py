ebec999260778620027c38b40d3566e5133bd759
salwarmoghal <salwar098@gmail.com>
2023-11-29 23:50:38 +0530

    redirecting apply button to a new page

--- app.py
+++ app.py
@@ -10,8 +10,17 @@ def hello_world():
   cnx,cur=database.loadDB()
   cur.execute("SELECT * FROM rgukt.jobs")
   JOBS=cur.fetchall()
-  print(JOBS)
   return render_template('home.html',jobs=JOBS)
+
+@app.route("/job/<id>")
+def show_job(id):
+  cnx,cur=database.loadDB()
+  qurey = "SELECT * FROM rgukt.jobs WHERE id=" + id
+  cur.execute(qurey)
+  job1=cur.fetchone()
+  if not job1:
+    return "Not Found",404
+  return render_template('jobpage.html',job=job1)
 
 if __name__=="__main__":
   app.run(host='0.0.0.0', debug=True)
